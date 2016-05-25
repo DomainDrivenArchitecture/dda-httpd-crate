7cda1d00ba81c7977f68f3687068f868de577b25
Jan Krebs <jan.krebs@meissa-gmbh.de>
2016-05-25 12:35:00 +0200

    * added eclipse .project and leiningen

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject org.domaindrivenarchitecture/httpd "0.0.1-SNAPSHOT"
+  :description "new dda-httpd-crate"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.palletops/pallet "0.8.12"]
+                 [prismatic/schema "1.1.1"]
+                 ;[metosin/schema-tools "0.10.0-SNAPSHOT"]
+                 ]
+  :repositories [["snapshots" :clojars]
+                 ["releases" :clojars]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]]
+  :profiles {:dev
+             {:dependencies
+              [[com.palletops/pallet "0.8.12" :classifier "tests"]
+               [org.clojure/test.check "0.9.0"]]
+              :plugins
+              [[com.palletops/pallet-lein "0.8.0-alpha.1"]
+               [lein-ancient "0.6.10"]]}
+             :leiningen/reply
+             {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.21"]]
+              :exclusions [commons-logging]}}
+  :local-repo-classpath true
+  )
