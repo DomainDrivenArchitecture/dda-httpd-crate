db9857ca2612682b03bad838196b22027494d9ed
Jan Krebs <jan.krebs@meissa-gmbh.de>
2016-05-26 16:13:35 +0200

    fixed issues leading to compilation errors

--- src/org/domaindrivenarchitecture/pallet/crate/httpd.clj
+++ src/org/domaindrivenarchitecture/pallet/crate/httpd.clj
@@ -1,18 +1,33 @@
-; TODO: review jem: 2016_05_25: copyright comment is missing.
+; Licensed to the Apache Software Foundation (ASF) under one
+; or more contributor license agreements. See the NOTICE file
+; distributed with this work for additional information
+; regarding copyright ownership. The ASF licenses this file
+; to you under the Apache License, Version 2.0 (the
+; "License"); you may not use this file except in compliance
+; with the License. You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
 
 (ns org.domaindrivenarchitecture.pallet.crate.httpd
   (:require
     [schema.core :as s]
-     [schema-tools.core :as st]
+    [schema-tools.core :as st]
+    [org.domaindrivenarchitecture.config.commons.map-utils :as map-utils]
   ))
 
-; TODO: review jem: 2016_05_25: Naming convention is HttpdConfig
-(def httpdConfig
+
+(def HttpdConfig
   "defines a schema for a httpdConfig"
   {:fqdn s/Str
    :app-port s/Str
    :maintainance-page-content [s/Str]
-   ; review jem: 2016_05_25: parts from liferay was missing!   
+   (s/optional-key :httpd)
    (s/conditional
        #(= (:letsencrypt %) true)
        {:letsencrypt (s/eq true) 
@@ -32,8 +47,8 @@
         (s/optional-key :maintainance-page-content) [s/Str]})
    })
 
-; TODO: review jem: 2016_05_25: name should be default-sth 
-(def httpd-webserver-configuration
+
+(def default-httpd-webserver-configuration
   {; Webserver Configuration
    :letsencrypt true
    :fqdn "localhost.localdomain"
@@ -41,3 +56,12 @@
    :maintainance-page-content ["<h1>Webserver Maintainance Mode</h1>"]})
 
 ; TODO: review jem: 2016_05_25: we will need some merge-config function.
+(s/defn ^:always-validate merge-config :- HttpdConfig
+  "merges the partial config with default config & ensures that resulting config is valid."
+  [partial-config]
+  (map-utils/deep-merge default-httpd-webserver-configuration partial-config))
+
+
+
+
+
