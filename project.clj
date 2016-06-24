20a372b0b599e1647644d2cc68a2c44600402513
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-06-24 08:51:33 +0200

    updated deps & integrated in masterbuild

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "new dda-httpd-crate"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.palletops/pallet "0.8.12"]
-                 [prismatic/schema "1.1.1"]
+                 [prismatic/schema "1.1.2"]
                  [org.domaindrivenarchitecture/dda-config-commons "0.1.4"]
                  [org.domaindrivenarchitecture/dda-pallet "0.1.0-SNAPSHOT"]
                  [org.domaindrivenarchitecture/httpd "0.2.3-SNAPSHOT"]]
