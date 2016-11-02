0515227038ed2342a63bd2073469d1756f58ef5d
Jan Krebs <jan.krebs@meissa-gmbh.de>
2016-11-02 14:45:27 +0100

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.domaindrivenarchitecture/dda-httpd-crate "0.1.1-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-httpd-crate "0.1.0"
   :description "new dda-httpd-crate"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.palletops/pallet "0.8.12" :exclusions [org.clojure/tools.cli]]
                  [prismatic/schema "1.1.3"]
-                 [org.domaindrivenarchitecture/dda-init-crate "0.1.0-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-config-commons "0.1.5"]
-                 [org.domaindrivenarchitecture/dda-pallet "0.1.0"]
-                 [org.domaindrivenarchitecture/httpd "0.2.3-SNAPSHOT"]]
+                 [org.domaindrivenarchitecture/dda-init-crate "0.1.1"]
+                 [org.domaindrivenarchitecture/dda-config-commons "0.1.6"]
+                 [org.domaindrivenarchitecture/dda-pallet "0.1.1"]
+                 [org.domaindrivenarchitecture/httpd "0.2.3"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -16,7 +16,7 @@
                              "intergation" "integration"]
               :dependencies
               [[com.palletops/pallet "0.8.12" :classifier "tests"]
-               [org.domaindrivenarchitecture/dda-config-commons "0.1.5" :classifier "tests"]
+               [org.domaindrivenarchitecture/dda-config-commons "0.1.6" :classifier "tests"]
                [org.clojure/test.check "0.9.0"]]
               :plugins
               [[com.palletops/pallet-lein "0.8.0-alpha.1"]
