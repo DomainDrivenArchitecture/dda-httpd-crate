ccb2d8a8a85aad4e46d52f14c67a70b25be5ea6a
Jan Krebs <jan.krebs@meissa-gmbh.de>
2017-07-03 15:04:16 +0200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-httpd-crate "0.2.0-SNAPSHOT"
+(defproject dda/dda-httpd-crate "0.2.0"
   :description "new dda-httpd-crate"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
