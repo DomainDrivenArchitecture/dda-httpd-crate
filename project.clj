3c42cc2bb3352da0dd119d13e828b51555fb65e6
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-07-14 11:27:09 +0200

    lein-release plugin: bumped version from 0.2.1 to 0.2.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-httpd-crate "0.2.1"
+(defproject dda/dda-httpd-crate "0.2.2-SNAPSHOT"
   :description "new dda-httpd-crate"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
