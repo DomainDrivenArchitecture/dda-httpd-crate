088ace26fa9e2eeb2757f59577293c6add4ceadd
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-10-16 08:59:28 +0200

    updateded deps

--- project.clj
+++ project.clj
@@ -4,16 +4,14 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.1.6"]
-                 [dda/dda-config-commons "0.2.2"]
-                 [dda/dda-pallet "0.5.4"]
+                 [dda/dda-pallet "0.5.5-SNAPSHOT"]
                  [dda/httpd "0.2.7"]]
   :profiles {:dev {:source-paths ["integration"]
                    :resource-paths ["dev-resources"]
                    :dependencies
                    [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                     [com.palletops/pallet "0.8.12" :classifier "tests"]
-                    [dda/dda-config-commons "0.2.2" :classifier "tests"]
+                    [dda/dda-config-commons "0.2.3-SNAPSHOT" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.2.3"]
                     [org.slf4j/jcl-over-slf4j "1.8.0-alpha2"]]
                    :plugins
