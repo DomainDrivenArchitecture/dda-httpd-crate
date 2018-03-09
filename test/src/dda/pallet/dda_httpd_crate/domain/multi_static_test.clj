bad16d5613a3b918a11cf9cd8cb99b216afb66bc
jem <michael.jerger@meissa-gmbh.de>
2018-03-09 12:51:23 +0100

    fixed ns name

--- test/src/dda/pallet/dda_httpd_crate/domain/multi_static_test.clj
+++ test/src/dda/pallet/dda_httpd_crate/domain/multi_static_test.clj
@@ -13,7 +13,7 @@
 ; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
-(ns dda.pallet.dda-httpd-crate.domain.multi--static-test
+(ns dda.pallet.dda-httpd-crate.domain.multi-static-test
   (:require
    [clojure.test :refer :all]
    [schema.core :as s]
