f277a9c4007ea3f7b223989971d51ecfb2a20647
jem <michael.jerger@meissa-gmbh.de>
2018-03-09 08:23:15 +0100

    add license header

--- main/src/dda/pallet/dda_httpd_crate/domain/compatibility_domain.clj
+++ main/src/dda/pallet/dda_httpd_crate/domain/compatibility_domain.clj
@@ -1,3 +1,18 @@
+; Licensed to the Apache Software Foundation (ASF) under one
+; or more contributor license agreements. See the NOTICE file
+; distributed with this work for additional information
+; regarding copyright ownership. The ASF licenses this file
+; to you under the Apache License, Version 2.0 (the
+; "License"); you may not use this file except in compliance
+; with the License. You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
 (ns dda.pallet.dda-httpd-crate.domain.compatibility-domain
   (:require
    [dda.pallet.dda-httpd-crate.infra :as infra]
@@ -40,6 +55,7 @@
                               (s/optional-key :max-clients) s/Int}
     (s/optional-key :apache-modules) {(s/optional-key :a2enmod) [s/Str]}}})
 
+; TODO: add schema description to function signature
 (defn create-vhost-stack-config-from-domain
   "Creates a vhost stack configuration from a vhost-convention config."
   [vhost-convention-config]
@@ -53,6 +69,7 @@
               (:access-control vhost-convention-config)
               ["Order allow,deny" "Allow from all" ""]))))
 
+; TODO: add schema description to function signature
 (defn create-stack-vhost-config
   "Creates a stack vhost configuration from a httpd-domain-config."
   [httpd-domain-config]
@@ -60,13 +77,14 @@
    (for [[k v] (:vhosts httpd-domain-config)]
      {k (create-vhost-stack-config-from-domain v)})))
 
-
+; TODO: add schema description to function signature
 (defn create-stack-limits
   "Creates stack-limits-configuration"
   [convention-config]
   {:server-limit (or (-> convention-config :limits :server-limit) 150)
    :max-clients (or (-> convention-config :limits :max-clients) 150)})
 
+; TODO: add schema description to function signature
 (defn create-stack-jk-configuration
   "Creates the stack-jk-configuration"
   [convention-config]
