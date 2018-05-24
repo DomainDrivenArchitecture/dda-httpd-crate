edc65b2d88ba5c62aba846f88bdf5ff5111cc641
jem <michael.jerger@meissa-gmbh.de>
2018-05-24 17:25:04 +0200

    Version 2.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-httpd-crate "2.0.6"
+(defproject dda/dda-httpd-crate "2.0.7-SNAPSHOT"
   :description "new dda-httpd-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
