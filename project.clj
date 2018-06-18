0f0d5b8604c3508afcb20facf9be5030021f2bf9
Jan Krebs <jan.krebs@meissa-gmbh.de>
2018-06-18 18:22:13 +0200

    Upgrade to pallet with clojure 1.9. Exclude com.palletops/pallet in pallet-aws dependency.

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "2.1.2"]
-                 [dda/httpd "0.2.7"]]
+  :dependencies [[dda/dda-pallet "2.2.0" :exclusions [org.clojure/tools.reader]]
+                 [dda/httpd "0.2.9" :exclusions [org.clojure/tools.reader]]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -18,27 +17,26 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [
-                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
-                    [com.palletops/pallet "0.8.12" :classifier "tests"]
-                    [org.clojure/tools.cli "0.3.7"]
-                    [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
+                   [[org.clojure/tools.cli "0.3.7"]
+                    [ch.qos.logback/logback-classic "1.2.3"]
+                    [org.slf4j/jcl-over-slf4j "1.7.25"]
+                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"
+                                                        :exclusions [com.palletops/pallet]]]
                    :plugins [[lein-sub "0.3.0"]
-                             [lein-pprint "1.1.2"]]
-                   :repl-options {:init-ns dda.pallet.dda-httpd-crate.app.instantiate-aws}
+                             [lein-pprint "1.2.0"]]
                    :leiningen/reply {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
-                                     :exclusions [commons-logging]}}
+                                     :exclusions [commons-logging]}
+                   :repl-options {:init-ns dda.pallet.dda-git-crate.app.instantiate-aws}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[com.palletops/pallet "0.8.12" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-httpd-crate.main
                        :dependencies [[org.clojure/tools.cli "0.3.7"]
-                                      [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
+                                      [ch.qos.logback/logback-classic "1.2.3"]
+                                      [org.slf4j/jcl-over-slf4j "1.7.25"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
